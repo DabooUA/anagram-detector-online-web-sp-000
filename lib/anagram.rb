ef74f79891246569861d5137547d91c82b74a6a6
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-08 15:01:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   def anagram_search(word_array)
     word = []
     sorted_words = anagram_word.split("").sort
-    
+
     word_array.each do |w_a|
       word << w_a
       if w_a.split("").sort == sorted_words
