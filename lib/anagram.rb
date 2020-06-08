b15f87e3e66cfc4b523864eb8c278d53bc01c502
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-08 15:02:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,8 +11,7 @@ class Anagram
     sorted_words = anagram_word.split("").sort
 
     word_array.each do |w_a|
-      word << w_a
-      if w_a.split("").sort == sorted_words
+      word << w_a if w_a.split("").sort == sorted_words
       end
       word
     end
